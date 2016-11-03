a6627e9451af76190524b1c3883418b538ffb5b3
anatoly <tolitius@gmail.com>
2016-11-03 18:13:29 -0400

    hooking the first rum component

--- hubble/src/cljs/hubble/app.cljs
+++ hubble/src/cljs/hubble/app.cljs
@@ -2,15 +2,18 @@
     (:require [rum.core :as rum]
               [hubble.utils :as u]))
 
-(rum/defc label [text]
+(def log (atom []))
+
+(rum/defc inside-hubble < rum/reactive []
   [:div
-   [:h1 "A label"]
-   [:p text]])
+   [:h1 "Inside Hubble"]
+   [:p (rum/react log)]])
 
 (defn read-news [news]
-  (println "news from the server:" (u/unpack news)))
+  (println "news from the server:" (u/unpack news))
+  (swap! log conj (str (js/Date.) (u/unpack news))))
 
 (defn init []
   (u/connect (str "ws://" (.-host js/location) "/ws")
              read-news)
-  (rum/mount (label) (u/by-id "container")))
+  (rum/mount (inside-hubble) (u/by-id "container")))
