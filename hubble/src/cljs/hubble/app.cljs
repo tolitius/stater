fcaf61c35ca6b10bb76eeca921362465f9a05b84
anatoly <tolitius@gmail.com>
2016-11-06 21:23:19 -0500

    rum components: many global atoms to no global state at all

--- hubble/src/cljs/hubble/app.cljs
+++ hubble/src/cljs/hubble/app.cljs
@@ -2,25 +2,24 @@
     (:require [rum.core :as rum]
               [hubble.utils :as u]))
 
-(def log (atom (sorted-map-by >)))
-(def mission (atom {}))
-(def store (atom {}))
-(def camera (atom {}))
+(rum/defc space-log < rum/reactive [hubble]
+  (let [log (rum/cursor-in hubble [:log])]
+    [:div (for [[ts entry] (rum/react log)]
+            [:div.log-entry
+             [:span.ts (u/ms->sdate ts)]
+             [:span.entry entry]])]))
 
-(rum/defc space-log < rum/reactive []
-  [:div (for [[ts entry] (rum/react log)]
-          [:div.log-entry
-           [:span.ts (u/ms->sdate ts)]
-           [:span.entry entry]])])
+(rum/defc hubble-store < rum/reactive [hubble]
+  (let [store (rum/cursor-in hubble [:store])]
+    [:div [:p.component-name "store"]
+     [:img {:src (as-> (rum/react store) $
+                   (get-in $ [:connected-to :url])
+                   (u/img-path "store" $))}]]))
 
-(rum/defc hubble-store < rum/reactive []
-  [:div [:p.component-name "store"]
-   [:img {:src (as-> (rum/react store) $
-                 (get-in $ [:connected-to :url])
-                 (u/img-path "store" $))}]])
-
-(rum/defc hubble-mission < rum/reactive []
-  (let [color (if (= "mono" (-> (rum/react camera)
+(rum/defc hubble-mission < rum/reactive [hubble]
+  (let [mission (rum/cursor-in hubble [:mission])
+        camera (rum/cursor-in hubble [:camera])
+        color (if (= "mono" (-> (rum/react camera)
                                 :settings :mode))
                 "grayscale"
                 "in-color")]
@@ -29,42 +28,50 @@
                      :target
                      (u/img-path "mission"))}]))
 
-(rum/defc hubble-camera < rum/reactive []
-  [:div [:p.component-name "camera"]
-   [:img {:src (as-> (rum/react camera) $
-                 (get-in $ [:settings :mode])
-                 (u/img-path "camera" $))}]])
+(rum/defc hubble-camera < rum/reactive [hubble]
+  (let [camera (rum/cursor-in hubble [:camera])]
+    [:div [:p.component-name "camera"]
+     [:img {:src (as-> (rum/react camera) $
+                   (get-in $ [:settings :mode])
+                   (u/img-path "camera" $))}]]))
 
-(rum/defc recalibrate < rum/reactive []
-  (let [next-mission (rum/react mission)]
+(rum/defc recalibrate < rum/reactive [hubble]
+  (let [mission (rum/cursor-in hubble [:mission])
+        next-mission (rum/react mission)]
     [:img {:class "recal"
            :src "img/repoint-hubble.gif"}]))
 
-(defn repoint-hubble [state]
+(defn repoint-hubble [hubble state]
   (let [{:keys [details]} state
         melem (u/by-id "mission")]
-    (rum/mount (recalibrate) melem)
+    (rum/mount (recalibrate hubble) melem)
     (js/setTimeout (fn []
-                     (reset! mission details)
+                     (swap! hubble assoc :mission details)
                      (rum/unmount melem)
-                     (rum/mount (hubble-mission) melem)) 3000)))
+                     (rum/mount (hubble-mission hubble) melem)) 3000)))
 
-(defn read-news [msg]
+(defn read-news [hubble msg]
   (let [{:keys [name state]} (u/unpack msg)
         component (u/short-name name)]
     (println "news from the server:" [component state])
     (when (not= component :config)
       (case component
-        :mission (repoint-hubble state)
-        :store (reset! store state)
-        :camera (reset! camera state))
-      (swap! log assoc (u/now)
-                       (str {component (dissoc state :active)})))))
+        :mission (repoint-hubble hubble state)
+        :store (swap! hubble assoc :store state)
+        :camera (swap! hubble assoc :camera state))
+      (swap! hubble update :log assoc (u/now)
+                                      (str {component (dissoc state :active)})))))
 
 (defn init []
-  (u/connect (str "ws://" (.-host js/location) "/ws")
-             read-news)
-  (rum/mount (space-log) (u/by-id "space-log"))
-  (rum/mount (hubble-store) (u/by-id "store"))
-  (rum/mount (hubble-camera) (u/by-id "camera"))
-  (rum/mount (hubble-mission) (u/by-id "mission")))
+  (let [hubble (atom {:log (sorted-map-by >)
+                      :mission {}
+                      :store {}
+                      :camera {}})]
+
+    (u/connect (str "ws://" (.-host js/location) "/ws")
+               (partial read-news hubble))
+
+    (rum/mount (space-log hubble) (u/by-id "space-log"))
+    (rum/mount (hubble-store hubble) (u/by-id "store"))
+    (rum/mount (hubble-camera hubble) (u/by-id "camera"))
+    (rum/mount (hubble-mission hubble) (u/by-id "mission"))))
